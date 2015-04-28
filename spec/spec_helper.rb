3bad72527d22df83d779d7ddf920b05a9204a505
flatonfloor <flatonfloor@gmail.com>
2015-04-28 09:26:00 +0500

    Finish layout and routes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,13 +4,11 @@ require 'spork'
 #require 'spork/ext/ruby-debug'
 
 Spork.prefork do
-  # Loading more in this block will cause your tests to run faster. However,
-  # if you change any configuration or code from libraries loaded here, you'll
-  # need to restart spork for it take effect.
   ENV["RAILS_ENV"] ||='test'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
   require 'rspec/autorun'
+  require 'capybara/rspec'
 
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
   ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
